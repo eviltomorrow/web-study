5ba193d3833c71fb6231c3561d647c903ac8fe4d
活祖宗 <shepard@liarsa.localdomain>
2024-01-01 08:23:41 +0800

    dev

--- javascript/chapter03/example.js
+++ javascript/chapter03/example.js
@@ -4,3 +4,33 @@
 10000
 0xff
 0xCAFE011
+
+// 浮点型直接量
+3.14
+2345.789
+.3333
+6.02e23
+1.4738E-32
+
+// 算数运算
+Infinity
+Number.POSITIVE_INFINITY
+1/0
+Number.MAX_VALUE + 1
+Number.NEGATIVE_INFINITY
+-Infinity
+-1/0
+-Number.MAX_VALUE - 1
+NaN
+Number.NaN
+0/0
+Number.MIN_VALUE/2
+-Number.MIN_VALUE/2
+-1/Infinity
+-0
+
+// 类型转换
+10 + " objects";
+"7" * "4";
+var n = 1 - "x";
+n + " objects";
